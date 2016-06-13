ca9ec21ea030a37fef942215ba5e9b4d638a2b67
shalurai <shaluraics22@gmail.com>
2016-06-13 14:04:38 +0900

    Add array adapter

--- app/src/main/java/com/shablcu/shalu/sunshineapp/MainActivity.java
+++ app/src/main/java/com/shablcu/shalu/sunshineapp/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -37,6 +38,12 @@ public class MainActivity extends AppCompatActivity {
             };
             List<String> weekForecast = new ArrayList<String>(Arrays.asList(data));
 
+
+            ArrayAdapter<String>mForecastAdapter = new ArrayAdapter<String>(
+                            getActivity(),
+                            R.layout.list_item_forcast,
+                            R.id.list_item_forecast_textview,
+                            weekForecast);
 
             View rootView = inflater.inflate(R.layout.fragment_blank, container, false);
             return rootView;
