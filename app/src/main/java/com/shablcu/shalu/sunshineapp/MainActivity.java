1f680319d2c5c4fbfc2487c9cd71b70ad5bba0bf
shalurai <shaluraics22@gmail.com>
2016-06-13 13:57:34 +0900

    Add fake data

--- app/src/main/java/com/shablcu/shalu/sunshineapp/MainActivity.java
+++ app/src/main/java/com/shablcu/shalu/sunshineapp/MainActivity.java
@@ -7,6 +7,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -16,13 +20,26 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public static class PlaceholderFragment extends Fragment {
-              public PlaceholderFragment() {
-                  }
-                @Override
-                public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                         Bundle savedInstanceState) {
-                    View rootView = inflater.inflate(R.layout.fragment_blank, container, false);
-                 return rootView;
-          }
-            }
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            String[] data = {
+                    "Mon 6/23 - Sunny - 31/17",
+                    "Tue 6/24 - Foggy - 21/8",
+                    "Wed 6/25 - Cloudy - 22/17",
+                    "Thurs 6/26 - Rainy - 18/11",
+                    "Fri 6/27 - Foggy - 21/10",
+                    "Sat 6/28 - TRAPPED IN WEATHERSTATION - 23/18",
+                    "Sun 6/29 - Sunny - 20/7"
+            };
+            List<String> weekForecast = new ArrayList<String>(Arrays.asList(data));
+
+
+            View rootView = inflater.inflate(R.layout.fragment_blank, container, false);
+            return rootView;
+        }
+    }
 }
