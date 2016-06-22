3ac6b20c0a358f3e7ac9bca88ecc73b5ccbe4e62
shalurai <shaluraics22@gmail.com>
2016-06-22 14:24:12 +0900

    Add location from preference

--- app/src/main/java/com/shablcu/shalu/sunshineapp/ForCastFragment.java
+++ app/src/main/java/com/shablcu/shalu/sunshineapp/ForCastFragment.java
@@ -61,13 +61,8 @@ public class ForCastFragment extends Fragment {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
         if (id == R.id.action_refresh) {
-            FetchWeatherTask weatherTask = new FetchWeatherTask();
 
-            SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getActivity());
-                        String location = prefs.getString(getString(R.string.pref_location_key),
-                                       getString(R.string.pref_location_default));
-            weatherTask.execute(location);
-
+            updateWeather();
             return true;
         }
         return super.onOptionsItemSelected(item);
@@ -104,17 +99,35 @@ public class ForCastFragment extends Fragment {
         // Get a reference to the ListView, and attach this adapter to it.
         ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
         listView.setAdapter(mForecastAdapter);
-     listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-         @Override
-         public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
-             Intent intent = new Intent(getActivity(), DetailActivity.class);
-             String forcast=mForecastAdapter.getItem(position);
-             intent.putExtra(Intent.EXTRA_TEXT, forcast);
-             startActivity(intent);
-         }
-     });
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                Intent intent = new Intent(getActivity(), DetailActivity.class);
+                String forcast = mForecastAdapter.getItem(position);
+                intent.putExtra(Intent.EXTRA_TEXT, forcast);
+                startActivity(intent);
+            }
+        });
         return rootView;
     }
+
+
+    private void updateWeather() {
+        FetchWeatherTask weatherTask = new FetchWeatherTask();
+
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getActivity());
+        String location = prefs.getString(getString(R.string.pref_location_key),
+                getString(R.string.pref_location_default));
+        weatherTask.execute(location);
+
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        updateWeather();
+    }
+
 
     public class FetchWeatherTask extends AsyncTask<String, Void, String[]> {
 
@@ -144,7 +157,7 @@ public class ForCastFragment extends Fragment {
         /**
          * Take the String representing the complete forecast in JSON Format and
          * pull out the data we need to construct the Strings needed for the wireframes.
-         * <p/>
+         * <p>
          * Fortunately parsing is easy:  constructor takes the JSON string and converts it
          * into an Object hierarchy for us.
          */
