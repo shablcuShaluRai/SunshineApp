8a0b37d5e7d231f76b8b3cd5c43d8fd2e9d602c7
shalurai <shaluraics22@gmail.com>
2016-06-24 15:58:32 +0900

    Add location database

--- app/src/main/java/com/shablcu/shalu/sunshineapp/data/WeatherDbHelper.java
+++ app/src/main/java/com/shablcu/shalu/sunshineapp/data/WeatherDbHelper.java
@@ -17,6 +17,14 @@ public class WeatherDbHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+        final String SQL_CREATE_LOCATION_TABLE = "CREATE TABLE " + WeatherContract.LocationEntry.TABLE_NAME + " (" +
+                                WeatherContract.LocationEntry._ID + " INTEGER PRIMARY KEY," +
+                                WeatherContract.LocationEntry.COLUMN_LOCATION_SETTING + " TEXT UNIQUE NOT NULL, " +
+                                WeatherContract.LocationEntry.COLUMN_CITY_NAME + " TEXT NOT NULL, " +
+                               WeatherContract.LocationEntry.COLUMN_COORD_LAT + " REAL NOT NULL, " +
+                                WeatherContract.LocationEntry.COLUMN_COORD_LONG + " REAL NOT NULL " +
+                                " );";
         final String SQL_CREATE_WEATHER_TABLE = "CREATE TABLE " + WeatherContract.WeatherEntry.TABLE_NAME + " (" +
                 // Why AutoIncrement here, and not above?
                 // Unique keys will be auto-generated in either case.  But for weather
@@ -49,6 +57,7 @@ public class WeatherDbHelper extends SQLiteOpenHelper {
                 WeatherContract.WeatherEntry.COLUMN_LOC_KEY + ") ON CONFLICT REPLACE);";
 
         sqLiteDatabase.execSQL(SQL_CREATE_WEATHER_TABLE);
+        sqLiteDatabase.execSQL(SQL_CREATE_LOCATION_TABLE);
     }
 
     @Override
