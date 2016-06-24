fbdfa1b99942c508b86c52cdc189a45f4f8cfb24
shalurai <shaluraics22@gmail.com>
2016-06-24 13:42:07 +0900

    add constant in contract

--- app/src/main/java/com/shablcu/shalu/sunshineapp/data/WeatherContract.java
+++ app/src/main/java/com/shablcu/shalu/sunshineapp/data/WeatherContract.java
@@ -23,6 +23,18 @@ public class WeatherContract {
     public static final class LocationEntry implements BaseColumns {
         public static final String TABLE_NAME = "location";
 
+       public  static final String COLUMN_LOCATION_SETTING = "location_setting";
+
+                        // Human readable location string, provided by the API.  Because for styling,
+                        // "Mountain View" is more recognizable than 94043.
+                        public static final String COLUMN_CITY_NAME = "city_name";
+
+                        // In order to uniquely pinpoint the location on the map when we launch the
+                        // map intent, we store the latitude and longitude as returned by openweathermap.
+                        public static final String COLUMN_COORD_LAT = "coord_lat";
+                         public static final String COLUMN_COORD_LONG = "coord_long";
+
+
     }
 
     /* Inner class that defines the table contents of the weather table */
