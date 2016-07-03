d5174f7dbc257b1e21fb09b326b53c182a6cdf25
shalurai <shaluraics22@gmail.com>
2016-07-03 15:35:24 +0900

    Two item view types

--- app/src/main/java/com/shablcu/shalu/sunshineapp/ForecastAdapter.java
+++ app/src/main/java/com/shablcu/shalu/sunshineapp/ForecastAdapter.java
@@ -14,12 +14,25 @@ public class ForecastAdapter extends CursorAdapter {
        public ForecastAdapter(Context context, Cursor c, int flags) {
               super(context, c, flags);
           }
-
+    private static final int VIEW_TYPE_COUNT = 2;
+    private static final int VIEW_TYPE_TODAY = 0;
+    private static final int VIEW_TYPE_FUTURE_DAY = 1;
 
       @Override
         public View newView(Context context, Cursor cursor, ViewGroup parent) {
-           View view = LayoutInflater.from(context).inflate(R.layout.list_item_forcast, parent, false);
-         return view;
+          int viewType = getItemViewType(cursor.getPosition());
+                 int layoutId = -1;
+                 switch (viewType) {
+                     case VIEW_TYPE_TODAY: {
+                          layoutId = R.layout.list_item_forecast_today;
+                        break;
+                   }
+                 case VIEW_TYPE_FUTURE_DAY: {
+                     layoutId = R.layout.list_item_forcast;
+                      break;
+                }
+                  }
+                  return LayoutInflater.from(context).inflate(layoutId, parent, false);
         }
 
         /*
@@ -55,4 +68,16 @@ public class ForecastAdapter extends CursorAdapter {
                   double low = cursor.getDouble(ForecastFragment.COL_WEATHER_MIN_TEMP);
                     TextView lowView = (TextView) view.findViewById(R.id.list_item_low_textview);
                     lowView.setText(Utility.formatTemperature(low, isMetric));
-        }		     }
+        }
+
+    @Override
+       public int getItemViewType(int position) {
+        return position == 0 ? VIEW_TYPE_TODAY : VIEW_TYPE_FUTURE_DAY;
+       }
+       @Override
+       public int getViewTypeCount() {
+          return VIEW_TYPE_COUNT;
+   }
+
+}
+
